0a8b1baf75231ca84adb6fcf62969746d81105f3
aparnans <aparna_ns@yahoo.com>
2019-07-23 20:14:02 -0400

    adding comments

---
+++ data.js
@@ -0,0 +1,7 @@
+// This file will add, modify or delete data from the project database.
+// Create a new database connection
+// Create a function to accept new user input
+// // input validation function
+// // Store data into the database
+// // // Encrypt the user password
+// // // // function to encrypt the password  
