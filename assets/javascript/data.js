4560010e0a8340907becaeedb3d913aa14fe3ee6
aparnans <aparna_ns@yahoo.com>
2019-07-29 23:44:43 -0400

    Added searchcriteria and images to cards

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -15,7 +15,8 @@ var client_secret = "JDx7nY5jRhNXP0UPmG7YbwE2OSqlOvSrB0urVCab"; //Secret
 var accessToken = "";
 var refreshTokenAttempted = false;
 
-var foundPet=false;
+var foundPet = false;
+var noOfResults = 0;
 
 // Create a firebase object
 var firebaseConfig = {
@@ -29,9 +30,11 @@ var firebaseConfig = {
 
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 // create a database ref variable
-var database = firebase.database();  
+var database = firebase.database();
 // temporary code to create initial data
 var usersObjArray =
     [
@@ -68,35 +71,35 @@ var usersObjArray =
                     }
                 }]
 
-    
-  },    
-  {
-    name:'PQR',  
-    address:'456 street, Chicago, IL',
-    email:'a@b.com',
-    petPreference : {
-        type:'cat',breed:'Tabby',gender:'female',color:'Orange',altered:'Yes'
-      }
-  }
 
-  ];
+        },
+        {
+            name: 'PQR',
+            address: '456 street, Chicago, IL',
+            email: 'a@b.com',
+            petPreference: {
+                type: 'cat', breed: 'Tabby', gender: 'female', color: 'Orange', altered: 'Yes'
+            }
+        }
+
+    ];
 
 //   log('after creating array');
 //   log('name: '+usersObjArray[0].name)
-  
-  function addRow(pUsersObjArray){
-        //log('in addRow');
-        // open a loop on the objects array
-        for(var i=0;i<pUsersObjArray.length;i++){
-            //log('current index : '+i);
-            //log('address: '+pUsersObjArray[i].address);
-            // add a row to the database
-            database.ref('/UserID-'+i).set(
-                pUsersObjArray[i]
-            );
-            //log('after database set');
-        }
-    
+
+function addRow(pUsersObjArray) {
+    //log('in addRow');
+    // open a loop on the objects array
+    for (var i = 0; i < pUsersObjArray.length; i++) {
+        //log('current index : '+i);
+        //log('address: '+pUsersObjArray[i].address);
+        // add a row to the database
+        database.ref('/UserID-' + i).set(
+            pUsersObjArray[i]
+        );
+        //log('after database set');
+    }
+
     // Add search history to the user
     database.ref(pUserID + '/searchHistory/' + cnt).set(searchHistoryObj);
 };
@@ -198,26 +201,47 @@ function refreshToken() {
 }
 
 var searchPetObj = {
-    type: 'dog',
-    breed: 'Australian Shepherd',
-    color: 'Tricolor (Brown, Black, & White)',
-    gender: 'female',
+    type: 'cat',
+    breed: '',
+    color: '',
+    gender: 'male',
     location: {
-        city: 'Nashville',
-        state: 'TN',
-        zip: '37201'
+        city: '',
+        state: 'KS',
+        zip: ''
     }
 };
+// This function will check the user entries against the result set obtained in the ajax call
+function isConditionTrue(userParam,resultVal){
+    log('userParam',userParam.toUpperCase());
+    log('resultVal',resultVal.toUpperCase());
+    if(userParam!=''){
+        if(userParam.toUpperCase()===resultVal.toUpperCase()){
+            userParam='';
+            resultVal='';
+            return true;
+        }
+    }else{
+        log('userParam is null')    
+        userParam='';
+            resultVal='';
+            return true;
+    }
+    userParam='';
+            resultVal='';
+            return false; 
+} // end of isConditionTrue
+
 function search(searchPetObj) {
-    log('searchPetObj ',searchPetObj);
-     
+    log('searchPetObj ', searchPetObj);
+
     // set up a a query variable
     var queryURL = "https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/animals";
     // add parameteres to the queryURL
     queryURL = queryURL + '?';
     // Add type of animal
     queryURL = queryURL + 'type=' + searchPetObj.type;
-    
+
     $.ajax({
         url: queryURL,
         method: "GET",
@@ -228,46 +252,66 @@ function search(searchPetObj) {
         log('in ajax - search');
         //log('in search ajax animals ',result);
         // loop through animals array to add more filters 
-        var result=response.animals;
-        log('result:',result);
-        $(result).each(function(index){
-            log('Breed ',result[index].breeds.primary.toUpperCase());
-            //log('parameter breed ',searchPetObj.breed.toUpperCase());
-            //log('selected animal breed ',result[index].breeds.primary.toUpperCase());
+        var result = response.animals;
+        log('result:', result);
+        $(result).each(function (index) {
+            //log('Breed ', result[index].breeds.primary.toUpperCase());
+            //log('parameter breed ', searchPetObj.breed.toUpperCase());
+            foundPet = false;
             // Check the breed
-             if(result[index].breeds.primary.toUpperCase() === searchPetObj.breed.toUpperCase()){
-                log('selected animal breed ',result[index].breeds.primary.toUpperCase());
-                // now check for color
-                if(result[index].colors.primary.toUpperCase() === searchPetObj.color.toUpperCase()){
-                    //now check for gender
-                    if(result[index].gender.toUpperCase() === searchPetObj.gender.toUpperCase()){
-                        // check location
-                        //Check state
-                        if(result[index].contact.address.state.toUpperCase() === searchPetObj.location.state.toUpperCase()){
-                            //Check city
-                            if(result[index].contact.address.city.toUpperCase() === searchPetObj.location.city.toUpperCase()){
+            if(isConditionTrue(searchPetObj.breed , result[index].breeds.primary)){
+                log('now check the color');
+                log('sending',searchPetObj.color, ' and ',result[index].colors.primary);
+                if(isConditionTrue(searchPetObj.color , result[index].colors.primary)){
+                    log('now check the gender');
+                    if(isConditionTrue(searchPetObj.gender , result[index].gender)){
+                        log('check location');
+                        log('Check state');
+                        if(isConditionTrue(searchPetObj.location.state , result[index].contact.address.state)){
+                            log('Check city');
+                            if(isConditionTrue(searchPetObj.location.city , result[index].contact.address.city)){
                                 //check zip
-                                log('result[index].contact.address.postcode',result[index].contact.address.postcode);
-                                if(result[index].contact.address.postcode.toUpperCase() === searchPetObj.location.zip.toUpperCase()){
+                                if(isConditionTrue(searchPetObj.location.zip , result[index].contact.address.postcode)){
                                     log('found the dog');
-                                    foundPet=true;
-                                    var petImage=$("<img></img>");
-                                    petImage.attr('src',result[index].photos[0].small);
-                                    $("#result-list").prepend(petImage);
+                                    foundPet = true;
+                                    noOfResults++;
+                                    // add image to a card
+                                    var divCol3 = $('<div class = "col-sm-3" ></div>');
+                                    var divCard = $('<div class = "card"></div>');
+                                    var divCardBody = $('<div class="card-body"></div>');
+                                    var divH5 = $('<h5 class="card-title">' + result[index].name + '</h5>');
+                                    divH5.css('width','100%','text-align','center');
+                                    var petImage = $("<img></img>");
+                                    petImage.attr('src', result[index].photos[0].small);
+                                    petImage.css('width','100%');
+                                    var aTag = $('<a href="' + result[index].url + '" class="btn btn-primary">More about me</a>');
+                                    aTag.css('width','100%','text-align','center');
+                                    divCardBody.append(divH5);
+                                    divCardBody.append(petImage);
+                                    divCardBody.append(aTag);
+                                    divCard.append(divCardBody);
+                                    divCol3.append(divCard);
+                                    $('.petImageHolder').append(divCol3);
                                 }
                             }
                         }
                     }
                 }
-
-             }
+            }
         })
-        if(!foundPet){
+        if (!foundPet) {
             log('did not find the pet');
+            log('noOfResults',noOfResults);
+            
+            if(noOfResults<6 && noOfResults>=0){
+                log('Recursive search');
+                //search(searchPetObj);
+            }
+            
         }
-        
+
     }).catch(function (err) {
-        console.log("ERROR! ", err);
+        //console.log("ERROR! ", err);
         // call refreshtoken if the token is expired or asked for the first time
         if (err.responseJSON.status === 401 && !refreshTokenAttempted) {
             refreshToken();
@@ -277,37 +321,37 @@ function search(searchPetObj) {
 
 search(searchPetObj);
 // On click function to pull city names based on state
-$(document).on("click", "#dropdown-state", function(){
+$(document).on("click", "#dropdown-state", function () {
 
     // Ajax pull for json file
     $.ajax({
         type: "GET",
-        url:"./assets/media/csvjson.json",
-        }).then((resp)=>{
-            console.log(resp);
+        url: "./assets/media/csvjson.json",
+    }).then((resp) => {
+        console.log(resp);
 
-            // Delete child elements of city dropdown
-            $("#dropdown-city").empty();
+        // Delete child elements of city dropdown
+        $("#dropdown-city").empty();
 
-            // Add placeholder to city dropdown
-            $("#dropdown-city").append("<option>Choose...</option>");
+        // Add placeholder to city dropdown
+        $("#dropdown-city").append("<option>Choose...</option>");
 
-            // capture value of state value
-            var stateVal = $("#dropdown-state").val();
-            console.log(stateVal);
+        // capture value of state value
+        var stateVal = $("#dropdown-state").val();
+        console.log(stateVal);
 
-            $.each(resp, function(index, value) {
-                if( resp[index].state_id === stateVal ){
+        $.each(resp, function (index, value) {
+            if (resp[index].state_id === stateVal) {
 
-                    // Set variables to create new options for city
-                    var cityDropdown = $("#dropdown-city");
-                    var newCityOption = $("<option>");
-                    
-                    // Append new options for city for each city in the state
-                    cityDropdown.append(newCityOption.text(resp[index].city).attr({
-                        value: resp[index].state_id,
-                    }))
-                }
-            })
+                // Set variables to create new options for city
+                var cityDropdown = $("#dropdown-city");
+                var newCityOption = $("<option>");
+
+                // Append new options for city for each city in the state
+                cityDropdown.append(newCityOption.text(resp[index].city).attr({
+                    value: resp[index].state_id,
+                }))
+            }
         })
+    })
 });    
